57ba77da2460fba65ea8ed428a5094cbc80c7f74
Hernandez Angel Alberto <admahernandez@ti529.local>
2026-02-23 13:13:59 -0600

    primer commit

---
+++ assets/js/modalAuto.js
@@ -0,0 +1,6 @@
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const modalElement = document.getElementById('staticBackdrop');
+    const modal = new bootstrap.Modal(modalElement);
+    modal.show();
+  });
---
+++ assets/js/type.js
@@ -0,0 +1,6 @@
+window.ityped.init(document.querySelector('.ityped'),{
+    strings: ['¡ Asiste !'],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+})
